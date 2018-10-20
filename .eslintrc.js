6337971a6b34ee83d1a221878d5435d2d6781892
BANO.notIT <bano.notit@yandex.ru>
2018-10-20 23:15:03 +0300

    Change eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,14 +4,42 @@ module.exports = {
         node: true,
     },
     extends: [
+        'eslint:recommended',
         'plugin:vue/essential',
-        '@vue/airbnb',
     ],
     rules: {
         'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+
+        'eol-last': [
+            'error',
+            'always'
+        ],
+        'curly': [
+            'error',
+            'all'
+        ],
+        'indent': [
+            2,
+            4,
+            {
+                'SwitchCase': 1
+            }
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'never'
+        ]
     },
     parserOptions: {
         parser: 'babel-eslint',
     },
-};
+}
